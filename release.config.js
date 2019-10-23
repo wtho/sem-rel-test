618d644bcb5cb21923c66f6e8a2d85cb79cfb915
Thomas Wirth <thomas@wtho.de>
2019-10-23 16:40:35 +0200

    fix: typo in release.config.js

--- release.config.js
+++ release.config.js
@@ -1,9 +1,9 @@
-{
+exports.module = {
   plugins: [
     "@semantic-release/commit-analyzer",
     "@semantic-release/release-notes-generator",
     "@semantic-release/changelog",
     "@semantic-release/npm",
     "@semantic-release/github"
-  ];
+  ]
 }
